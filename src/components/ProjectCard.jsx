b1c8a17a4ddca739b9a2c4ee7630945f14286b03
Tharuka <tmttthennakoon@std.foc.sab.ac.lk>
2026-04-20 00:54:39 +0530

    Add main card container styling

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,8 +1,17 @@
+
 export default function ProjectCard({ project, index }) {
   return (
-    <div>
-      <h3>{project.title}</h3>
-      <p>{project.description}</p>
+    <div
+      className="
+        relative
+        bg-[#16121f]
+        border border-purple-900/40
+        rounded-2xl
+        p-10
+        shadow-lg shadow-purple-900/50
+        transition-all duration-300
+      "
+    >
     </div>
   );
 }
